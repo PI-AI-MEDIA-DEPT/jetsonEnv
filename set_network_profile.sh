559ae9152b32e04b69c3caf4074f30a7b68bcf45
kbj0222 <82368948+kbj0222@users.noreply.github.com>
2025-08-13 21:48:50 +0900

    Update set_network_profile.sh

--- set_network_profile.sh
+++ set_network_profile.sh
@@ -8,16 +8,16 @@ GATEWAY="$3"
 DNS="$4"
 
 # static-ip 프로필 삭제 후 생성
-nmcli connection delete static-ip >/dev/null 2>&1
-nmcli connection add type ethernet con-name static-ip ifname "$(ip route | grep '^default' | awk '{print $5}')" \
+sudo mcli connection delete static-ip >/dev/null 2>&1
+sudo nmcli connection add type ethernet con-name static-ip ifname "$(ip route | grep '^default' | awk '{print $5}')" \
     ipv4.addresses "$STATIC_IP/$PREFIX" \
     ipv4.gateway "$GATEWAY" \
     ipv4.dns "$DNS" \
     ipv4.method manual
 
 # dhcp-ip 프로필 없으면 생성
-if ! nmcli connection show | grep -q "^dhcp-ip"; then
-    nmcli connection add type ethernet con-name dhcp-ip ifname "$(ip route | grep '^default' | awk '{print $5}')" \
+if ! sudo nmcli connection show | grep -q "^dhcp-ip"; then
+    sudo nmcli connection add type ethernet con-name dhcp-ip ifname "$(ip route | grep '^default' | awk '{print $5}')" \
         ipv4.method auto
     echo "✅ DHCP 프로필 생성 완료"
 else
